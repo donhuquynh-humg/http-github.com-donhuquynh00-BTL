28ef8d02b77ce00a5cf41c81bc858b1cd4b9cb34
donhuquynh-humg <77618944+donhuquynh-humg@users.noreply.github.com>
2021-05-17 21:43:08 +0700

    Add files via upload

---
+++ ChatLieusController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyBanHang.Models;
+
+namespace QuanLyBanHang.Controllers
+{
+    public class ChatLieusController : Controller
+    {
+        private LTQLDbContext db = new LTQLDbContext();
+
+        // GET: ChatLieus
+        public ActionResult Index()
+        {
+            return View(db.ChatLieus.ToList());
+        }
+
+        // GET: ChatLieus/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChatLieu chatLieu = db.ChatLieus.Find(id);
+            if (chatLieu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chatLieu);
+        }
+
+        // GET: ChatLieus/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ChatLieus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,MaChatLieu,TenChatLieu")] ChatLieu chatLieu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ChatLieus.Add(chatLieu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(chatLieu);
+        }
+
+        // GET: ChatLieus/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChatLieu chatLieu = db.ChatLieus.Find(id);
+            if (chatLieu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chatLieu);
+        }
+
+        // POST: ChatLieus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,MaChatLieu,TenChatLieu")] ChatLieu chatLieu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(chatLieu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(chatLieu);
+        }
+
+        // GET: ChatLieus/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ChatLieu chatLieu = db.ChatLieus.Find(id);
+            if (chatLieu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chatLieu);
+        }
+
+        // POST: ChatLieus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ChatLieu chatLieu = db.ChatLieus.Find(id);
+            db.ChatLieus.Remove(chatLieu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ HangHoasController.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using QuanLyBanHang.Models;
+
+namespace QuanLyBanHang.Controllers
+{
+    public class HangHoasController : Controller
+    {
+        private LTQLDbContext db = new LTQLDbContext();
+
+        // GET: HangHoas
+       
+        public ActionResult Index()
+        {
+            return View(db.HangHoas.ToList());
+        }
+
+        // GET: HangHoas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HangHoa hangHoa = db.HangHoas.Find(id);
+            if (hangHoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hangHoa);
+        }
+
+        // GET: HangHoas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HangHoas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaHangHoa,TenHangHoa,MaChatLieu,SoLuong,DonGiaNhap,DonGiaBan")] HangHoa hangHoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HangHoas.Add(hangHoa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(hangHoa);
+        }
+
+        // GET: HangHoas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HangHoa hangHoa = db.HangHoas.Find(id);
+            if (hangHoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hangHoa);
+        }
+
+        // POST: HangHoas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaHangHoa,TenHangHoa,MaChatLieu,SoLuong,DonGiaNhap,DonGiaBan")] HangHoa hangHoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hangHoa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(hangHoa);
+        }
+
+        // GET: HangHoas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HangHoa hangHoa = db.HangHoas.Find(id);
+            if (hangHoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hangHoa);
+        }
+
+        // POST: HangHoas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            HangHoa hangHoa = db.HangHoas.Find(id);
+            db.HangHoas.Remove(hangHoa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DownloadFile()
+        {
+            //duong dan chua file muon download
+            string path = AppDomain.CurrentDomain.BaseDirectory + "Uploads/Excels/"; // tu muc chua fiel excel
+            //chuyen file sang dang byte
+            byte[] fileBytes = System.IO.File.ReadAllBytes(path + "fielexcel.xlsx"); //doan nay de file excel
+            //ten file khi download ve
+            string fileName = "Sinhviencapnhat.xlsx";
+            //tra ve file
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+
+            string _FileName = "hanghoa.xls";
+
+            string _path = Path.Combine(Server.MapPath("~/Uploads/Excels"), _FileName);
+
+            file.SaveAs(_path);
+
+            DataTable dt = ReadDataFromExcelFile(_path);
+            /* CopyDataByBulk(dt);*/
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                HangHoa kr = new HangHoa();
+                kr.MaHangHoa = dt.Rows[i][0].ToString();
+                kr.TenHangHoa = dt.Rows[i][1].ToString();
+                kr.MaChatLieu = dt.Rows[i][2].ToString();
+                kr.SoLuong = dt.Rows[i][3].ToString();
+                kr.DonGiaNhap = dt.Rows[i][4].ToString();
+                kr.DonGiaBan = dt.Rows[i][5].ToString();
+               
+
+                db.HangHoas.Add(kr);
+                db.SaveChanges();
+            }
+
+            /* CopyDataByBulk(excel.ReadDataFromExcelFile(_path));*/
+            return RedirectToAction("Index");
+        }
+
+        private void CopyDataByBulk(object v)
+        {
+            throw new NotImplementedException();
+        }
+        public DataTable ReadDataFromExcelFile(string filepath)
+        {
+            string connectionString = "";
+            string fileExtention = filepath.Substring(filepath.Length - 4).ToLower();
+            if (fileExtention.IndexOf("xlsx") == 0)
+            {
+                connectionString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source =" + filepath + ";Extended Properties=\"Excel 12.0 Xml;HDR=NO\"";
+            }
+            else if (fileExtention.IndexOf(".xls") == 0)
+            {
+                connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filepath + ";Extended Properties=Excel 8.0";
+            }
+
+
+            OleDbConnection oledbConn = new OleDbConnection(connectionString);
+            DataTable data = null;
+            try
+            {
+
+                oledbConn.Open();
+
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM [Sheet1$]", oledbConn);
+
+                OleDbDataAdapter oleda = new OleDbDataAdapter();
+
+                oleda.SelectCommand = cmd;
+
+                DataSet ds = new DataSet();
+
+                oleda.Fill(ds);
+
+                data = ds.Tables[0];
+            }
+            catch
+            {
+            }
+            finally
+            {
+
+                oledbConn.Close();
+            }
+            return data;
+        }
+
+        private void CopyDataByBulk(DataTable dt)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["LTQLDbContext"].ConnectionString);
+            SqlBulkCopy bulkcopy = new SqlBulkCopy(con);
+            bulkcopy.DestinationTableName = "HangHoas";
+            bulkcopy.ColumnMappings.Add(0, "MaHangHoa");
+            bulkcopy.ColumnMappings.Add(1, "TenHangHoa");
+            bulkcopy.ColumnMappings.Add(2, "MaChatLieu");
+            bulkcopy.ColumnMappings.Add(3, " SoLuong");
+            bulkcopy.ColumnMappings.Add(4, " DonGiaNhap");
+            bulkcopy.ColumnMappings.Add(5, "DonGiaBan");
+           
+            con.Open();
+            bulkcopy.WriteToServer(dt);
+            con.Close();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
