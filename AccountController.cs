128ab08233bad492cc2c81a6ad16158f4ed24182
donhuquynh-humg <77618944+donhuquynh-humg@users.noreply.github.com>
2021-05-17 21:42:05 +0700

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Security;
+using System.Web.Security;
+using QuanLyBanHang.Models;
+
+namespace QuanLyBanHang.Controllers
+{
+    public class AccountController : Controller
+    {
+        Encryption encry = new Encryption();
+       LTQLDbContext db = new LTQLDbContext();
+        // GET: Account
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(Account acc)
+        {
+            if (ModelState.IsValid)
+            {
+                acc.Password = encry.PasswordEncryption(acc.Password);
+                db.Accounts.Add(acc);
+                db.SaveChanges();
+                return RedirectToAction("Login", "Account");
+            }
+            return View(acc);
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Account acc)
+        {
+            if (ModelState.IsValid)
+            {
+                string encryptionpass = encry.PasswordEncryption(acc.Password);
+                var model = db.Accounts.Where(m => m.UserName == acc.UserName && m.Password == encryptionpass).ToList().Count();
+                if (model == 1)
+                {
+                    FormsAuthentication.SetAuthCookie(acc.UserName, true);
+                    return RedirectToAction("Index", "HangHoas");
+                   
+                }
+                else
+                {
+                    ModelState.AddModelError(" ", "Thông Tin Đăng Nhập Không Chính Xác");
+                }
+            }
+            return View(acc);
+        }
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+
+
+    }
+}
